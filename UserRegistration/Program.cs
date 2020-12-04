434f287ec4d075d9edf3d9b96f90274ac54ecb7a
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-12-04 14:45:34 +0530

    [Arti Dandge] Add : Ask User to enter Last name and validate last name

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -4,12 +4,17 @@ namespace UserRegistration
 {
     class Program
     {
+       
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registration Project !");
             Console.WriteLine("Enter First Name");
             string first_name = Console.ReadLine();
-            Console.WriteLine(UserRegistration.validateFirstName(first_name));
+            Boolean result = UserRegistration.validateFirstName(first_name);
+            Console.WriteLine(result);
+            Console.WriteLine("Enter Last Name");
+            string last_name = Console.ReadLine();
+            Console.WriteLine(UserRegistration.validateFirstName(last_name));
         }
     }
 }
