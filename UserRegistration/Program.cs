ed960207cc70281efd7ea4071e01b983d09a4fce
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-12-04 13:41:31 +0530

    [Arti Dandge] Add : Ask User to enter first name and validate it

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -7,6 +7,9 @@ namespace UserRegistration
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registration Project !");
+            Console.WriteLine("Enter First Name");
+            string first_name = Console.ReadLine();
+            Console.WriteLine(UserRegistration.validateFirstName(first_name));
         }
     }
 }
---
+++ UserRegistration/UserRegistration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    class UserRegistration
+    {
+
+        public static bool validateFirstName(string name)
+        {   string expression = "^[A-Z]{1}[a-zA-Z]{2,}";
+            Regex rg = new Regex(expression);   
+                return rg.IsMatch(name);
+        }
+    }
+}
